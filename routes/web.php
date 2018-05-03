baa50e78269ef0cbe3bd2be86d27ef7409988b2a
scorewinner <tim@webkeller.ch>
2018-05-03 09:42:51 +0200

    Tutorial Lesson 8

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
-Route::get('/tasks', function () {
-    $tasks =  App\Task::all();
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function($id){
-    $task = App\Task::find($id);
-    App\Task::incomplete();
-    return view('tasks.show', compact('task'));
-});
