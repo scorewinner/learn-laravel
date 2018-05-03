6fc81cd596ff9b02cf37ad82b4d986a6bbb7430c
scorewinner <tim@webkeller.ch>
2018-05-03 16:57:09 +0200

    Comment section Lesson 15

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
         <div class="col-sm-8">
-            <a class="blog-header-logo text-dark" href="#">My Blog</a>
+            <a class="blog-header-logo text-dark" href="/">My Blog</a>
         </div>
         <div class="col-sm-4 d-flex justify-content-end align-items-center">
             <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,5 +5,21 @@
         <h1>{{ $post->title }}</h1>
 
         <p>{{ $post->body }}</p>
+
+        <hr>
+
+        <div class="comments">
+            <h4>Comments:</h4>
+            <ul class="list-group">
+            @foreach($post->comments as $comment)
+                <li class="list-group-item">
+                    <strong>
+                        {{ $comment->created_at->diffForHumans() }}:
+                    </strong>
+                    {{ $comment->body }}
+                </li>
+            @endforeach
+            </ul>
+        </div>
     </div>
 @endsection
