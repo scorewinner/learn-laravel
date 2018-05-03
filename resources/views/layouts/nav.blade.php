e7a62e6400ba2547992baf77326adc8094026e85
scorewinner <tim@webkeller.ch>
2018-05-03 11:04:48 +0200

    Tutorial lesson 11 create blog

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,10 @@
+<div class="navbar navbar-dark bg-dark box-shadow">
+    <div class="container d-flex justify-content-between">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+            <strong>Album</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
+Route::get('/', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
