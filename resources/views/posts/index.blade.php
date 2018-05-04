e977a8ee0af4707f686fffbf32fde5a019f510f9
scorewinner <tim@webkeller.ch>
2018-05-04 11:45:02 +0200

    short design hotfix

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <div class="col-md-8 blog-main">
-        <h3 class="pb-3 mb-4 font-italic border-bottom">From the Firehose</h3>
 
         @foreach($posts as $post)
             @include('posts.post')
